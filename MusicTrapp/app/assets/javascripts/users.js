44222147f49f4b83353a28f60471f7d8f30db7f4
Shriyan Gunasekera <shriyan.gunasekera@gmail.com>
2015-07-03 00:22:07 -0400

    completed clicking on related artist functionality

--- MusicTrapp/app/assets/javascripts/users.js
+++ MusicTrapp/app/assets/javascripts/users.js
@@ -5,36 +5,42 @@ $(document).ready(function(){
 
 var eventBindings = function(){
   // $('#new_playlist').on('submit', newPlaylist)
-  $('#search-form').on('submit', newSearch)
+  $('#search-form').on('submit', newSearch);
+  $('#search-results-container').on('click', '.related-artist-avatar', clickSearch);
+
 }
 
-// var newPlaylist = function(event){
-//   event.preventDefault();
-
-//   var data = $('#new_playlist').serialize();
-//   var url = $('#new_playlist').attr('action')
-
-//   $('#new-playlist-container').hide();
-//   $('#search-container').show();
-
-//   $.ajax({
-//     method: 'post',
-//     url: url,
-//     data: data,
-//     dataType: 'json'
-//   }).done(function(response){
-//     console.log(response)
-//     var source = $('#playlists-template').html();
-//     var template = Handlebars.compile(source);
-//     $('#playlists-list').append(template(response));
-
-//   })
-// }
 
 var artistId = ""
 
 var newSearch = function(event){
   event.preventDefault();
+  var artist = $('#track_title').val().replace(/\s/g, "+");
+  var url = 'https://api.spotify.com/v1/search?q=' + artist + '&type=artist&limit=1'
+  var request = $.ajax({
+    method: 'get',
+    url: url
+  })
+  request.done(function(response){
+    console.log(response);
+    artistId = response.artists.items[0].id
+
+    var source = $('#artist-stuff').html();
+    var template = Handlebars.compile(source);
+    var context = response.artists.items[0]
+
+
+
+    $('#media-wrapper').prepend(template(context))
+
+    albumSearch();
+    relatedArtists();
+    })
+}
+
+var clickSearch = function(event){
+  event.preventDefault();
+  $('#track_title').val(event.target.attributes[1].value)
   var artist = $('#track_title').val().replace(/\s/g, "+");
   var url = 'https://api.spotify.com/v1/search?q=' + artist + '&type=artist&limit=1'
   var request = $.ajax({
@@ -112,30 +118,12 @@ var relatedArtists = function(){
     var template = Handlebars.compile(source);
     var context = {artists: topResults};
 
-    $(".artist-info").append(template(context))
+    artistInfoDom = "#artist-info-" + artistId
+
+    $(artistInfoDom).append(template(context));
+
   })
 
 }
 
 
-// var tracksSearch = function(){
-
-//   for(i=0; i < albums.length; i++){
-
-//       var albumId = albums[i].id
-//       albumDom = "#" + albumId
-//       var url = 'https://api.spotify.com/v1/albums/' + albumId + '/tracks?market=US'
-
-//       var request = $.ajax({
-//         method: 'get',
-//         url: url
-//       }).done(function(response){
-//         console.log(response)
-
-//         var source = $('#tracks-stuff').html();
-//         var template = Handlebars.compile(source);
-//         var context = {tracks: response.items}
-//         $(albumDom).append(template(context))
-//     })
-//   }
-// }
